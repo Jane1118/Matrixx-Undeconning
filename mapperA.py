655ffbc333cb135b580599a925e277ab5b93715a
hyperfraise <damien.menigaux@polytechnique.edu>
2017-02-28 16:50:05 +0100

    Update mapperA.py

--- mapperA.py
+++ mapperA.py
@@ -8,7 +8,17 @@ for line in sys.stdin:
     # remove leading and trailing whitespace
     i,j,v=line.split(' ')
     v=float(v)
-    print '%s\t%s' % (j,[i,'A',v])
+    
+    a = os.environ['mapreduce_map_input_file']
+    #a="/Users/dario/Dropbox/TEACHING-2016/X/BigData/Data/Tables/data/Customer.txt"
+    l = a.split("/")
+    namefile = l[len(l)- 1]
+    
+    if namefile=='A.txt':
+        print '%s\t%s' % (j,[i,'A',v])
+    elif namefile=='B.txt':
+        print '%s\t%s' % (i,[j,'A',v])
+
 
 
 
