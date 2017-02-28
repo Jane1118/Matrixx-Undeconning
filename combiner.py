e212f024277643db52b914f0a711efb45a53c6d4
hyperfraise <damien.menigaux@polytechnique.edu>
2017-02-28 16:31:47 +0100

    Create combiner.py

---
+++ combiner.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+import sys
+
+for line in sys.stdin:
+    pair = line.strip().split('\t')
+    s = pair[0]
+    l = pair[1]
+    #code du reducer
+    
+    if current_cid == cid:
+        lvalues.append(rest)
+
+    else:
+        if current_cid:
+            # write result to STDOUT
+            if 'Customer.txt' in lvalues :
+                for x in lvalues :
+                    if x != 'Customer.txt' :
+                        print '%s\t%s' % (current_cid, x.split(",")[1])
+
+        current_cid = cid
+        lvalues=[rest]
+
+if current_cid == cid:
+    if 'Customer.txt' in lvalues :
+        for x in lvalues :
+            if x != 'Customer.csv' :
+                    print '%s\t%s' % (cid, x.split(",")[1])
+
