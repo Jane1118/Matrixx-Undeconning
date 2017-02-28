8a4c9137cdcda443901feec2bfa59cd5b6626147
hyperfraise <damien.menigaux@polytechnique.edu>
2017-02-28 15:52:55 +0100

    Update MatrixMultiplication.py

--- MatrixMultiplication.py
+++ MatrixMultiplication.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Feb 28 14:57:54 2017
-D'abord on mappe des values de manière à obtenir après shuffle and sort des liste de maximum 2 values :
-max une de A d'indice *j et max une de B j*. Après on combine pour emit des produits de ces valeurs, 
+D'abord on mappe des values de manière à obtenir après shuffle and sort des listes de valeurs provenants de A d'indice *j
+et B d'indice j*. Après on combine pour emit des produits de ces valeurs, 
 les A(i,k)*B(k,j), et le reducer somme ces éléments selon kpour obtenir AB(i,j)
 
 @author: damien
@@ -13,7 +13,7 @@ def mapperA(file):
         i,j,v=list(line)
         v=float(v)
         for k in range(n):
-            emit(str(k)+j,[i,v])
+            emit(str(k)+j,[i,'A',v])
 
 
 def mapperB(file):
@@ -21,20 +21,25 @@ def mapperB(file):
         i,j,v=list(line)
         v=float(v)
         for k in range(n):
-            emit(str(k)+i,[j,v])
+            emit(str(k)+i,[j,'B',v])
 
 #s="uv" ou u identifie afin d'obtenir des couples, et
 #v est celui retrouve dans A(i,v)*B(v,j)
 def combiner(s,l):
-    if len(l)<=1:
-        c=0
-    else:
-        v1=l[0][1]
-        v2=l[1][1]
-        i=l[0][0]
-        j=l[1][0]
-        c=v1*v2
-        emit(str(i)+str(j),c)
+    As,Bs=[]
+    for x in l:
+        if x[1]=='A':
+            As.append(x)
+        else:
+            Bs.append(x)
+    for a in A:
+        v1=a[2]
+        i=a[0]
+        for b in B:
+            v2=b[2]
+            j=b[0]
+            c=v1*v2
+            emit(str(i)+str(j),c)
 
 def reducer(s,l):
     i,j=list(s)
