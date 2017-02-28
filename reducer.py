139887dddf112c3e7c653154acee9e947bce93f5
hyperfraise <damien.menigaux@polytechnique.edu>
2017-02-28 16:31:19 +0100

    Update reducer.py

--- reducer.py
+++ reducer.py
@@ -7,5 +7,28 @@ for line in sys.stdin:
     s = pair[0]
     l = pair[1]
     #code du reducer
+    
+    if current_cid == cid:
+        lvalues.append(rest)
+
+    else:
+        if current_cid:
+            # write result to STDOUT
+            if 'Customer.txt' in lvalues :
+                for x in lvalues :
+                    if x != 'Customer.txt' :
+                        print '%s\t%s' % (current_cid, x.split(",")[1])
+
+        current_cid = cid
+        lvalues=[rest]
+
+if current_cid == cid:
+    if 'Customer.txt' in lvalues :
+        for x in lvalues :
+            if x != 'Customer.csv' :
+                    print '%s\t%s' % (cid, x.split(",")[1])
+
+
+                                     
 
 
