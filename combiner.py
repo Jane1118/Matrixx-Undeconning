19a088805db31452784188445a64d9267de00a87
hyperfraise <damien.menigaux@polytechnique.edu>
2017-03-07 16:07:50 +0100

    Update combiner.py

--- combiner.py
+++ combiner.py
@@ -1,23 +1,38 @@
 #!/usr/bin/python
 
 import sys
+import ast
+
+current_cid = None
+cid = None
+As,Bs=[],[]
+
 
 for line in sys.stdin:
     pair = line.strip().split('\t')
-    s = pair[0]
-    l = pair[1]
+    cid = pair[0]
+    x=ast.literal_eval(pair[1])
     #code du reducer
-    As,Bs=[]
-    for x in l:
-        if x[1]=='A':
-            As.append(x)
-        else:
-            Bs.append(x)
-    for a in A:
-        v1=a[2]
-        i=a[0]
-        for b in B:
+    if current_cid != cid:
+        As,Bs=[],[]
+    current_cid = cid
+
+    
+    if x[1]=='A':
+        As.append(x)
+        v1=x[2]
+        i=x[0]        
+        for b in Bs:
             v2=b[2]
             j=b[0]
             c=v1*v2
-    print '%s\t%s' % (str(i)+str(j),c)
+            print '%s\t%s' % (str(i)+str(j),c)
+    else:
+        Bs.append(x)
+        v1=x[2]
+        j=x[0]        
+        for a in As:
+            v2=a[2]
+            i=a[0]
+            c=v1*v2
+            print '%s\t%s' % (str(i)+str(j),c)
