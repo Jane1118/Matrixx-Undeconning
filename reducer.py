adc56069db882e62abc9d54c924c73a590602cd0
hyperfraise <damien.menigaux@polytechnique.edu>
2017-03-07 16:08:52 +0100

    Update reducer.py

--- reducer.py
+++ reducer.py
@@ -2,17 +2,23 @@
 
 import sys
 
+current_cid = None
+cid = None
+c=0
+
 for line in sys.stdin:
-    pair = line.strip().split('\t')
-    s = pair[0]
-    l = pair[1]
-    #code du reducer
-    i,j=list(s)
-    v=0.0
-    for x in l:
-        v+=x
-    print '%s\t%s' % (i+' '+j+' '+str(v))
-
-                                     
-
+    cid,x = line.strip().split('\t', 1)
+    x=float(x[0])
+    print(cid,x)
+    if current_cid == cid:
+        c+=x
+    else:
+        if current_cid:
+            print '%s\t%s' % (current_cid[0]+' '+current_cid[1],str(c))
+            c=0
+            current_cid = cid
+    
+if current_cid == cid:
+    if current_cid:
+        print '%s\t%s' % (current_cid[0]+' '+current_cid[1],str(c))
 
